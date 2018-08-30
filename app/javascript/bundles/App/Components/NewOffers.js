416d12300771e3aaaf8620de97f8145feb8b4179
Whitney Lubin <whitneylubin@icloud.com>
2018-08-30 17:59:30 -0400

    changes to Newoffers and offercards

--- app/javascript/bundles/App/Components/NewOffers.js
+++ app/javascript/bundles/App/Components/NewOffers.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ReactOnRails from 'react-on-rails';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Button, Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
+import { Button, Card, CardBody, CardImage, CardTitle, CardText, Column, Row } from 'mdbreact';
 import Nav from './Nav.js';
 import OfferCards from './OfferCards.js';
 
@@ -10,11 +10,15 @@ class NewOffers extends React.Component {
 
     return (
       <div>
-        <Router>
-          <Nav isLoggedIn={this.props.isLoggedIn} />
-        </Router>
         <div>
-          <OfferCards />
+          <Router>
+            <Nav isLoggedIn={this.props.isLoggedIn} />
+          </Router>
+        </div>
+        <div>
+          <div className="d-flex justify-content-center">
+            <OfferCards />
+          </div>
         </div>
       </div>
     )
