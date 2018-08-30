8e47bb3435a983944249f2a3824f481b9ea19187
Whitney Lubin <whitneylubin@icloud.com>
2018-08-30 17:46:13 -0400

    attempt to update migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,35 +7,35 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2510809?id=xIiaI3&fmt=jpg&fit=constrain,1&wid=260&hei=278&op_sharpen=1&resMode=bilin
-# ​","brand_name"=>"MAC", "name"=>"Studio Fix Powder Plus Foundation", "size"=>"0.52 oz")
-#
-# Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2285068?id=xXMaz1&fmt=jpg&fit=constrain,1&wid=121&hei=361&op_sharpen=1&resMode=bilin
-# ​","brand_name"=>"Benefit Cosmetics", "name"=>"Roller Lash Super Curling & Lifting Mascara", "size"=>"0.3 oz")
-#
-# Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2501815?id=uRraU2&fmt=jpg&fit=constrain,1&wid=78&hei=359&op_sharpen=1&resMode=bilin
-# ​","brand_name"=>"Neutrogena", "name"=>"Hydroboost Hydrating Serum", "size"=>"1.0 oz")
-#
-# Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2529538?id=9cubT0&fmt=jpg&fit=constrain,1&wid=136&hei=360&op_sharpen=1&resMode=bilin
-# ​","brand_name"=>"Deva Curl", "name"=>"Leave-In Decadence", "size"=>"8.0 oz")
-#
-# Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2298792?id=EgUaV0&fmt=jpg&fit=constrain,1&wid=260&hei=204&op_sharpen=1&resMode=bilin
-# ​​","brand_name"=>"SheaMoisture", "name"=>"Superfruit Multi-Vitamin Frizz-Taming Smoothie", "size"=>"12.0 oz")
-#
-# Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2530608?id=4T1b-0&fmt=jpg&fit=constrain,1&wid=151&hei=360&op_sharpen=1&resMode=bilin
-# ​","brand_name"=>"ESSIE", "name"=>"Soda Pop Nail Polish Collection", "size"=>"0.46 oz")
-#
-# Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2307622?id=m3vaT3&fmt=jpg&fit=constrain,1&wid=128&hei=361&op_sharpen=1&resMode=bilin
-# ​","brand_name"=>"Dermalogica", "name"=>"Phyto Replenish Oil", "size"=>"1.0 oz")
-#
-# Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2273187?id=zQXaf1&fmt=jpg&fit=constrain,1&wid=260&hei=240&op_sharpen=1&resMode=bilin
-# ​","brand_name"=>"Completely Bare", "name"=>"Bikini Bump Blaster Ingrown Hair & Bikini Bump Eliminator")
-#
-# Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2292759?id=6udbs2&fmt=jpg&fit=constrain,1&wid=243&hei=360&op_sharpen=1&resMode=bilin
-# ​","brand_name"=>"Yves Saint Laurent", "name"=>"Black Opium Eau de Parfum", "size"=>"3.0 oz")
-#
-# Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2519432?id=zPoaR1&fmt=jpg&fit=constrain,1&wid=260&hei=171&op_sharpen=1&resMode=bilin
-# ​","brand_name"=>"Morphe", "name"=>"Jaclyn Hill Eyeshadow Palette", "size"=>"1.98 oz")
+Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2510809?id=xIiaI3&fmt=jpg&fit=constrain,1&wid=260&hei=278&op_sharpen=1&resMode=bilin
+​","brand_name"=>"MAC", "name"=>"Studio Fix Powder Plus Foundation", "size"=>"0.52 oz")
+
+Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2285068?id=xXMaz1&fmt=jpg&fit=constrain,1&wid=121&hei=361&op_sharpen=1&resMode=bilin
+​","brand_name"=>"Benefit Cosmetics", "name"=>"Roller Lash Super Curling & Lifting Mascara", "size"=>"0.3 oz")
+
+Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2501815?id=uRraU2&fmt=jpg&fit=constrain,1&wid=78&hei=359&op_sharpen=1&resMode=bilin
+​","brand_name"=>"Neutrogena", "name"=>"Hydroboost Hydrating Serum", "size"=>"1.0 oz")
+
+Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2529538?id=9cubT0&fmt=jpg&fit=constrain,1&wid=136&hei=360&op_sharpen=1&resMode=bilin
+​","brand_name"=>"Deva Curl", "name"=>"Leave-In Decadence", "size"=>"8.0 oz")
+
+Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2298792?id=EgUaV0&fmt=jpg&fit=constrain,1&wid=260&hei=204&op_sharpen=1&resMode=bilin
+​​","brand_name"=>"SheaMoisture", "name"=>"Superfruit Multi-Vitamin Frizz-Taming Smoothie", "size"=>"12.0 oz")
+
+Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2530608?id=4T1b-0&fmt=jpg&fit=constrain,1&wid=151&hei=360&op_sharpen=1&resMode=bilin
+​","brand_name"=>"ESSIE", "name"=>"Soda Pop Nail Polish Collection", "size"=>"0.46 oz")
+
+Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2307622?id=m3vaT3&fmt=jpg&fit=constrain,1&wid=128&hei=361&op_sharpen=1&resMode=bilin
+​","brand_name"=>"Dermalogica", "name"=>"Phyto Replenish Oil", "size"=>"1.0 oz")
+
+Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2273187?id=zQXaf1&fmt=jpg&fit=constrain,1&wid=260&hei=240&op_sharpen=1&resMode=bilin
+​","brand_name"=>"Completely Bare", "name"=>"Bikini Bump Blaster Ingrown Hair & Bikini Bump Eliminator")
+
+Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2292759?id=6udbs2&fmt=jpg&fit=constrain,1&wid=243&hei=360&op_sharpen=1&resMode=bilin
+​","brand_name"=>"Yves Saint Laurent", "name"=>"Black Opium Eau de Parfum", "size"=>"3.0 oz")
+
+Product.create("image"=>"https://images.ulta.com/is/image/Ulta/2519432?id=zPoaR1&fmt=jpg&fit=constrain,1&wid=260&hei=171&op_sharpen=1&resMode=bilin
+​","brand_name"=>"Morphe", "name"=>"Jaclyn Hill Eyeshadow Palette", "size"=>"1.98 oz")
 
 Retailer.create("name"=>"Sally Beauty Supply")
 Retailer.create("name"=>"Sephora")
