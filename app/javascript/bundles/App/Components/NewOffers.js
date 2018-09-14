8ef70525cebd90d5067b3a1a8ee58a2d59edd6e5
Whitney Lubin <whitneylubin@icloud.com>
2018-09-14 12:10:41 -0400

    add offergrid component

--- app/javascript/bundles/App/Components/NewOffers.js
+++ app/javascript/bundles/App/Components/NewOffers.js
@@ -3,7 +3,7 @@ import ReactOnRails from 'react-on-rails';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Button, Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
 import MyNav from './MyNav.js';
-import OfferCards from './OfferCards.js';
+import OfferGrid from './OfferGrid.js';
 
 
 
@@ -11,17 +11,17 @@ class NewOffers extends React.Component {
   render() {
 
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <Router>
-            <MyNav isLoggedIn={this.props.isLoggedIn} />
-          </Router>
-        </div>
-        <div className="container-fluid">
-          <div className="card-deck">
-            <OfferCards />
+        <div>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col">
+                <Router>
+                  <MyNav isLoggedIn={this.props.isLoggedIn} />
+                </Router>
+              </div>
+            </div>
           </div>
-        </div>
+        <OfferGrid />
       </div>
     )
   }
---
+++ app/javascript/bundles/App/Components/OfferGrid.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import ReactOnRails from 'react-on-rails';
+import { Grid, Row, Col, Container } from 'mdbreact';
+
+
+
+class OfferGrid extends React.Component {
+  render() {
+
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+              col
+          </div>
+          <div className="col">
+              col
+          </div>
+          <div className="col">
+              col
+          </div>
+          <div className="col">
+              col
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default OfferGrid;
